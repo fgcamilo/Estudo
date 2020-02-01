9126a84027a2e340a7e2e9c3bfde1253b6edc02d
fgcamilo <fgcamilo@gmail.com>
2020-02-01 20:15:52 -0300

    ddddddd
    dddd

--- PythonMegaCourse_udemy/the_basics/basics.py
+++ PythonMegaCourse_udemy/the_basics/basics.py
@@ -1,2 +1,65 @@
 import datetime
-print ("The date and time is", datetime.datetime.now())
+
+# data = datetime.datetime.now()
+# print ('The date and time is', data)
+# print ("Another way!!")
+# print (data)
+# mynumber = 10
+# mytext = "This is my text!!"
+# print (mynumber, mytext, data)
+
+# f = 10.54
+
+# print (type(mynumber), type(mytext), type(f))
+# rainfall = [10.1, 10, "my string in the first list", ["second list 1", "second list 2"]]
+
+# print ("")
+
+# print (rainfall)
+
+# student_grades = [99.1, 8.8, 7.5]
+# max_value = max(student_grades)
+# print(max_value)
+# student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+
+# print (student_grades.count(10.0))
+
+# username = "Python3"
+
+
+
+
+# print(type(username))
+
+# a  = ["a", "b", "c", "d"]
+# print (a)
+
+# monday_temperatures = [9.1, 8.8, 7.5]
+# student_grades = {"Mary": 9.1, 'Sim': 8.8, "John": 7.5}
+# print (student_grades['Sim'])
+
+# print (student_grades.values())
+# mysum = sum(student_grades.values())
+# length = len(student_grades)
+# mean = mysum / length
+# print (mean)
+
+# days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+# print (days[1:])
+
+# for x in range(0, 1000000000000000000000):
+#     print ('Te amo',x,'vezes POLAKA!!!!')
+# workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+# weekend = ["Sat", "Sun"]
+# #workdays.append(weekend.[0])
+# # print (calendar.
+# workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+# print (workdays[1:2])
+
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# print(letters[-3:])
+
+def mean(mylist):
+    the_mean = sum(mylist) / len(mylist)
+    return the_mean
+print (mean([8, 8]))
